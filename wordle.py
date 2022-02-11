6cdd413505b3e596bd3433c95e289e616b7584e4
thanai-mac <62784012+thanaism@users.noreply.github.com>
2022-02-11 10:07:56 +0900

    removed global access and made it a class

--- wordle.py
+++ wordle.py
@@ -2,12 +2,6 @@ import re
 import random
 import words
 
-word_list = words.word_list
-solution = random.choice(word_list).upper()
-pattern = re.compile("[a-zA-Z]{5}")
-loop_count = 0
-is_correct = False
-
 
 class Color:
     WHITE = "\33[37m"
@@ -17,54 +11,70 @@ class Color:
     UP = "\033[1A"
 
 
-def check_input(s):
-    n = len(s)
-    print(Color.UP, end="")
-    if n < 5:
-        print(s.upper() + " is too short.")
-        return False
-    if n > 5:
-        print(s.upper() + " is too long.")
-        return False
-    if not pattern.match(s):
-        print(s.upper() + " is non-alphabetic.")
-        return False
-    if s.lower() not in word_list:
-        print(s.upper() + " is not in word list.")
-        return False
-    return True
+class Game:
+    def __init__(self):
+        self.word_list = words.word_list
+        self.solution = random.choice(self.word_list).upper()
+        self.pattern = re.compile("[a-zA-Z]{5}")
+        self.loop_count = 0
+        self.is_correct = False
+        self.current_word = ""
+
+    def check_input(self):
+        s = self.current_word
+        n = len(s)
+        if n < 5:
+            print(s.upper() + " is too short.")
+            return False
+        if n > 5:
+            print(s.upper() + " is too long.")
+            return False
+        if not self.pattern.match(s):
+            print(s.upper() + " is non-alphabetic.")
+            return False
+        if s.lower() not in self.word_list:
+            print(s.upper() + " is not in word list.")
+            return False
+        return True
+
+    def judge(self):
+        s = self.current_word.upper()
+        output = ""
+        ok_count = 0
+        for i in range(5):
+            if s[i] == self.solution[i]:
+                output += Color.GREEN + s[i]
+                ok_count += 1
+            elif s[i] in self.solution:
+                output += Color.PURPLE + s[i]
+            else:
+                output += Color.WHITE + s[i]
+        output += Color.END
+        print(output)
+        return ok_count == 5
 
 
-def judge(s):
-    output = ""
-    ok_count = 0
-    for i in range(5):
-        if s[i] == solution[i]:
-            output += Color.GREEN + s[i]
-            ok_count += 1
-        elif s[i] in solution:
-            output += Color.PURPLE + s[i]
-        else:
-            output += Color.WHITE + s[i]
-    output += Color.END
-    print(output)
-    return ok_count == 5
+def main():
+    game = Game()
+    while True:
+        if game.loop_count == 6:
+            if game.is_correct:
+                print("Genius!")
+            else:
+                print("Try again.")
+            break
+
+        game.current_word = input()
+
+        print(Color.UP, end="")
+        if not game.check_input():
+            continue
+
+        game.loop_count += 1
+
+        if game.judge():
+            game.is_correct = True
 
 
-while True:
-    if loop_count == 6:
-        if is_correct:
-            print("Genius!")
-        else:
-            print("Try again.")
-        break
-
-    s = input()
-
-    if not check_input(s):
-        continue
-
-    loop_count += 1
-
-    if judge(s.upper()):
-        is_correct = True
+if __name__ == "__main__":
+    main()
