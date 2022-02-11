d60cc02330aa3b95f03c9fa7ecd0ea7e5513707a
thanai-mac <62784012+thanaism@users.noreply.github.com>
2022-02-11 12:29:41 +0900

    make variables private and functionize the while-loop

--- test_game.py
+++ test_game.py
@@ -1,7 +1,4 @@
 import unittest
-
-
-import unittest
 import wordle
 
 
@@ -9,19 +6,19 @@ class TestGame(unittest.TestCase):
     def test_check_input(self):
         game = wordle.Game()
 
-        game.current_word = "think"
+        game._current_word = "think"
         self.assertTrue(game.check_input())
 
-        game.current_word = "ANGLE"
+        game._current_word = "ANGLE"
         self.assertTrue(game.check_input())
 
-        game.current_word = "HOLT"
+        game._current_word = "HOLT"
         self.assertFalse(game.check_input())
 
-        game.current_word = "BEAUTIFUL"
+        game._current_word = "BEAUTIFUL"
         self.assertFalse(game.check_input())
 
-        game.current_word = "ET+AL"
+        game._current_word = "ET+AL"
         self.assertFalse(game.check_input())
 
     def test_gen_ouput(self):
@@ -31,24 +28,24 @@ class TestGame(unittest.TestCase):
         P = wordle.Color.PURPLE
         G = wordle.Color.GREEN
 
-        game.solution = "AAAAA"
-        game.current_word = "ABBBB"
+        game._solution = "AAAAA"
+        game._current_word = "ABBBB"
         output, ok_count = game.gen_output()
         self.assertTrue(output.count(W) == 4)
         self.assertTrue(output.count(P) == 0)
         self.assertTrue(output.count(G) == 1)
         self.assertEqual(ok_count, 1)
 
-        game.solution = "ABCDE"
-        game.current_word = "EDCBA"
+        game._solution = "ABCDE"
+        game._current_word = "EDCBA"
         output, ok_count = game.gen_output()
         self.assertTrue(output.count(W) == 0)
         self.assertTrue(output.count(P) == 4)
         self.assertTrue(output.count(G) == 1)
         self.assertEqual(ok_count, 1)
 
-        game.solution = "ABCDE"
-        game.current_word = "ABCDE"
+        game._solution = "ABCDE"
+        game._current_word = "ABCDE"
         output, ok_count = game.gen_output()
         self.assertTrue(output.count(W) == 0)
         self.assertTrue(output.count(P) == 0)
@@ -58,13 +55,24 @@ class TestGame(unittest.TestCase):
     def test_judge(self):
         game = wordle.Game()
 
-        game.solution = "ANGLE"
-        game.current_word = "ANGLE"
+        game._solution = "ANGLE"
+        game._current_word = "ANGLE"
         self.assertTrue(game.judge())
 
-        game.solution = "ANGLE"
-        game.current_word = "THINK"
+        game._solution = "ANGLE"
+        game._current_word = "THINK"
         self.assertFalse(game.judge())
+
+    def test_run(self):
+        game = wordle.Game()
+
+        game._loop_count = 6
+
+        game._is_correct = True
+        self.assertTrue(game.run())
+
+        game._is_correct = False
+        self.assertFalse(game.run())
 
 
 if __name__ == "__main__":
--- wordle.py
+++ wordle.py
@@ -16,15 +16,15 @@ class Color:
 
 class Game:
     def __init__(self: TGame) -> None:
-        self.word_list = words.word_list
-        self.solution = random.choice(self.word_list).upper()
-        self.pattern = re.compile("[a-zA-Z]{5}")
-        self.loop_count = 0
-        self.is_correct = False
-        self.current_word = ""
+        self._word_list = words.word_list
+        self._solution = random.choice(self._word_list).upper()
+        self._pattern = re.compile("[a-zA-Z]{5}")
+        self._loop_count = 0
+        self._is_correct = False
+        self._current_word = ""
 
     def check_input(self: TGame) -> bool:
-        s = self.current_word
+        s = self._current_word
         n = len(s)
         if n < 5:
             print(s.upper() + " is too short.")
@@ -32,10 +32,10 @@ class Game:
         if n > 5:
             print(s.upper() + " is too long.")
             return False
-        if not self.pattern.match(s):
+        if not self._pattern.match(s):
             print(s.upper() + " is non-alphabetic.")
             return False
-        if s.lower() not in self.word_list:
+        if s.lower() not in self._word_list:
             print(s.upper() + " is not in word list.")
             return False
         return True
@@ -46,41 +46,45 @@ class Game:
         return ok_count == 5
 
     def gen_output(self: TGame) -> Tuple[str, int]:
-        s = self.current_word.upper()
+        s = self._current_word.upper()
         output = ""
         ok_count = 0
         for i in range(5):
-            if s[i] == self.solution[i]:
+            if s[i] == self._solution[i]:
                 output += Color.GREEN + s[i]
                 ok_count += 1
-            elif s[i] in self.solution:
+            elif s[i] in self._solution:
                 output += Color.PURPLE + s[i]
             else:
                 output += Color.WHITE + s[i]
         output += Color.END
         return output, ok_count
 
+    def run(self):
+        while True:
+            if self._loop_count == 6:
+                if self._is_correct:
+                    print("Genius!")
+                    return True
+                else:
+                    print("Try again.")
+                    return False
+
+            self._current_word = input()
+
+            print(Color.UP, end="")
+            if not self.check_input():
+                continue
+
+            self._loop_count += 1
+
+            if self.judge():
+                self._is_correct = True
+
 
 def main() -> None:
     game = Game()
-    while True:
-        if game.loop_count == 6:
-            if game.is_correct:
-                print("Genius!")
-            else:
-                print("Try again.")
-            break
-
-        game.current_word = input()
-
-        print(Color.UP, end="")
-        if not game.check_input():
-            continue
-
-        game.loop_count += 1
-
-        if game.judge():
-            game.is_correct = True
+    game.run()
 
 
 if __name__ == "__main__":
